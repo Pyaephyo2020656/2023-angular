36eb8148b1ae4e6fe89bd7f4346b9d3e4145b9e4
Pyae Phyo Zaw <pyaephyoezaw.2019@gmail.com>
2023-08-26 16:48:00 +0630

    reactive-from

---
+++ reactive-driven-form/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SimpleComponent } from './simple/simple.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
