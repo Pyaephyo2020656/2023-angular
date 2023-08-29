9b9c7911f673d3b7c16ec3ef234b62f1a2657d7c
Pyae Phyo Zaw <pyaephyoezaw.2019@gmail.com>
2023-08-29 22:55:36 +0630

    pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pipes';
+
+
+  data1= 99
+  data2= 1.3
+  data3=0.1018
+}
